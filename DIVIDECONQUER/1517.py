4161ef091a5d0ff8259773bf78e4550f2d852a30
1996yyk <1996yyk@gmail.com>
2021-02-19 17:57:14 +0900

    Update 1517.py

--- DIVIDECONQUER/1517.py
+++ DIVIDECONQUER/1517.py
@@ -2,18 +2,18 @@ import sys
 
 N = int(sys.stdin.readline())
 A = list(map(int,sys.stdin.readline().split()))
-
-def bubble() :
-    global A
-    cnt = 0
-    for i in range(len(A)-1,-1,-1):
-        for j in range(i) :
-            if A[j] > A[j+1] :
-                cnt+=1
-                tmp = A[j]
-                A[j] = A[j+1]
-                A[j+1] = tmp
-
-    return cnt
-
-print(bubble())
+# 다시풀자
+# def bubble() :
+#     global A
+#     cnt = 0
+#     for i in range(len(A)-1,-1,-1):
+#         for j in range(i) :
+#             if A[j] > A[j+1] :
+#                 cnt+=1
+#                 tmp = A[j]
+#                 A[j] = A[j+1]
+#                 A[j+1] = tmp
+#
+#     return cnt
+#
+# print(bubble())
